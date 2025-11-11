bba5fd408b84b79c7bde113fc27f18ac5be832d1
Enrico Raphael Skoruppa <esdrifter@gmail.com>
2025-11-11 14:54:12 +0100

    added curvatures calculation

--- polycg/curv.py
+++ polycg/curv.py
@@ -5,19 +5,44 @@ import numpy as np
 from .gen_params import gen_params, gen_config
 from .cgnaplus import cgnaplus_bps_params
 
-def dnacurv(seq: str, disc: int, model: str = 'cgna+', angles: bool = True, degrees: bool = False) -> dict:
+def dnacurv(
+    seq: str,
+    disc_len: int, 
+    model: str = 'cgna+', 
+    angles: bool = True, 
+    degrees: bool = True
+    ) -> dict:
+    """_summary_
+
+    Args:
+        seq (str): DNA sequence 
+        disc_len (int): size of segments over which bend angles are calculated
+        model (str, optional): DNA structure and elasticity model. Defaults to 'cgna+'.
+        angles (bool, optional): express curvature as an angle. Defaults to True. If False, values are expressed as cos(theta).
+        degrees (bool, optional): Express angles in degrees. Defaults to True. Automatically False, if angles is False.
+
+    Returns:
+        numpy array 
+    """
     prms = gen_params(model,seq,composite_size=1)
     taus = gen_config(prms['gs'])
     N = len(seq)-1
-    M = N - disc + 1
+    M = N - disc_len + 1
     vals = np.zeros(M)
     for i in range(M):
-        vals[i] = np.dot(taus[i,:3,2],taus[i+disc,:3,2])
+        vals[i] = np.dot(taus[i,:3,2],taus[i+disc_len,:3,2])
     if angles:
         vals = np.arccos(vals)
-    if degrees:
+    if degrees and angles:
         vals = np.rad2deg(vals) 
-    return vals
+    curvdict = {
+        'curvature': vals, 
+        'sequence': seq, 
+        'disc_len': disc_len,
+        'model': model,
+        'angles': angles,
+        'degrees': degrees}
+    return curvdict
     
     
 if __name__ == "__main__":
