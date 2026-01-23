202e6e6609fc6bc5d240ecfa472a4f9001caeb86
Enrico Raphael Skoruppa <esdrifter@gmail.com>
2026-01-23 14:34:58 +0100

    latest

--- polycg/out/visualization.py
+++ polycg/out/visualization.py
@@ -5,11 +5,14 @@ import numpy as np
 from ..IOPolyMC import iopolymc as iopmc
 from ..genconf import gen_config
 
+# def visualize_chimerax(basefn: str, params: np.ndarray, seq: str, bead_radius: float = None, disc_len: float=0.34, include_bps_triads: bool = False):
+    
+
 def cgvisual(basefn: str, params: np.ndarray, seq: str, cg: int, startid: int = 0, bead_radius: float = None, disc_len: float=0.34, include_bps_triads: bool = False):
     
     # generate configuration
     taus = gen_config(params,disc_len=disc_len)
-
+    
     # # create folder
     # if not os.path.isdir(outdir):
     #     os.makedirs(outdir)
